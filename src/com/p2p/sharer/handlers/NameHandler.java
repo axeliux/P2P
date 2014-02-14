b799178efbaef3452d17b571b669328528249b74
Axel Velazquez <axel.david.velazquez@gmail.com>
2014-02-14 10:06:01 -0600

    Update NameHandler.java
    Handler Implementation

--- src/com/p2p/sharer/handlers/NameHandler.java
+++ src/com/p2p/sharer/handlers/NameHandler.java
@@ -4,19 +4,29 @@ import com.p2p.core.P2PMessage;
 import com.p2p.core.PeerConnection;
 import com.p2p.core.PeerHandler;
 import com.p2p.core.PeerNode;
+import com.p2p.sharer.SharerMessage;
 
+/**
+ * Handler for LIST Messages
+ * LIST: Requests a peer to reply with the list of peers that it knows about.
+ * @author axvelazq
+ */
+public class ListHandler extends PeerHandler {
 
-public class NameHandler extends PeerHandler{
-
-	public NameHandler(PeerNode node) {
+	public ListHandler(PeerNode node) {
 		super(node);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void handleMessage(PeerConnection conn, P2PMessage message) {
-		// TODO Auto-generated method stub
-
+		PeerNode peer = this.getInstanceNode();
+		conn.sendData(new P2PMessage(SharerMessage.REPLY,String.format("%d",peer.getNumberPeers())));
+		for(String pid : peer.getPeerKeys()){
+			String host = peer.getPeer(pid).getHost();
+			int port = peer.getPeer(pid).getPort();
+			conn.sendData(new P2PMessage(SharerMessage.REPLY,String.format("%s %s %d",pid,host,port)));
+		}
+		
 	}
 
 }
