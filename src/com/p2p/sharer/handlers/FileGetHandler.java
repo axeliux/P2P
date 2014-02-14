a5485112a8efb8086d6d2d4fc25a64e0a84c3aaf
Axel Velazquez <axel.david.velazquez@gmail.com>
2014-02-14 10:05:05 -0600

    Update FileGetHandler.java
    Handler Implementation

--- src/com/p2p/sharer/handlers/FileGetHandler.java
+++ src/com/p2p/sharer/handlers/FileGetHandler.java
@@ -1,11 +1,23 @@
 package com.p2p.sharer.handlers;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Hashtable;
+
 import com.p2p.core.P2PMessage;
 import com.p2p.core.PeerConnection;
 import com.p2p.core.PeerHandler;
 import com.p2p.core.PeerNode;
+import com.p2p.core.util.LoggerUtil;
+import com.p2p.sharer.SharerMessage;
+import com.p2p.sharer.SharerNode;
 
-
+/**
+ * Handler for the  FGET Messages
+ * FGET file-name: Request a peer to reply with the contents of the specified file.
+ * @author axvelazq
+ *
+ */
 public class FileGetHandler extends PeerHandler{
 
 	public FileGetHandler(PeerNode node) {
@@ -15,7 +27,27 @@ public class FileGetHandler extends PeerHandler{
 
 	@Override
 	public void handleMessage(PeerConnection conn, P2PMessage message) {
-		// TODO Auto-generated method stub
+		 String filename  = message.getMsgData();
+		 SharerNode peer = (SharerNode)this.getInstanceNode();
+		 Hashtable<String, String> files = peer.getTableFiles();
+		 if(!files.containsKey(filename)){
+			 conn.sendData(new P2PMessage(SharerMessage.ERROR, "FGET: FILE NOT FOUND" + filename));
+		 }
+		 
+		 byte [] filedata = null;
+		 try{
+			FileInputStream inFile = new FileInputStream(filename);
+			int length = inFile.available();
+			filedata = new byte[length];
+			inFile.read(filedata);
+			inFile.close();
+		 }catch(IOException ex){
+			 LoggerUtil.getLogger().severe("FGET: ERROR READING FILE: " + ex);
+			 conn.sendData(new P2PMessage(SharerMessage.ERROR,"FGET: ERROR READING FILE " + filename));
+			 return;
+		 }
+		 conn.sendData(new P2PMessage(SharerMessage.REPLY,filedata));
+		 
 
 	}
 
