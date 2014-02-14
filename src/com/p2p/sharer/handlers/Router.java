d3c1870f90d9c2b147276c73a09734c504883831
Axel Velazquez <axel.david.velazquez@gmail.com>
2014-02-14 10:08:19 -0600

    Update Router.java

--- src/com/p2p/sharer/handlers/Router.java
+++ src/com/p2p/sharer/handlers/Router.java
@@ -3,7 +3,11 @@ package com.p2p.sharer.handlers;
 import com.p2p.core.PeerInfo;
 import com.p2p.core.PeerNode;
 import com.p2p.core.interfaces.RouterInterface;
-
+/**
+ * Router
+ * @author axvelazq
+ *
+ */
 public class Router implements RouterInterface {
 	private PeerNode node;
 	public Router(PeerNode node){
@@ -11,8 +15,12 @@ public class Router implements RouterInterface {
 	}
 	@Override
 	public PeerInfo route(String peerId) {
-		// TODO Auto-generated method stub
-		return null;
+		if(node.getPeerKeys().contains(peerId)){
+			return node.getPeer(peerId);
+		}else{
+			return null;
+		}
+		
 	}
 
 }
